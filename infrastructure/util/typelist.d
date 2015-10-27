8cbf3a638e159ff929479be01b8471f57e124555
John Colvin <john.loughran.colvin@gmail.com>
2015-10-27 11:46:28 +0000

    clean up and fix infrastructure/util/typelist

--- infrastructure/util/typelist.d
+++ infrastructure/util/typelist.d
@@ -1,51 +1,53 @@
 module util.typelist;
 
-import std.algorithm;
-import std.typetuple;
-
-static if(__traits(hasMember,std.typetuple, "Filter")) {
-    public import std.typetuple: Filter;
-}else{
-    // functionality added in 2.061
-    template Filter(alias pred, lst...)
-    {
-        static if (lst.length > 0)
-        {
-            alias Filter!(pred, lst[1 .. $]) tail;
-            //
-            static if (pred!(lst[0]))
-                alias TypeTuple!(lst[0], tail) Filter;
-            else
-                alias tail Filter;
-        }
-        else
-            alias TypeTuple!() Filter;
-    }
-}
-
 template Join(string delimit, T...) {
         static if(T.length == 0) {
             enum Join = "";
         }else static if(T.length == 1) {
             enum Join =  T[0];
         }else {
-            enum Join = T[0] ~ "," ~ Join!(delimit,T[1..$]);
+            enum Join = T[0] ~ delimit ~ Join!(delimit,T[1..$]);
         }
 }
-
 
 // I rather like the whole algorithm("isThing(a)") shtick.
 // useful for templates too?
 template Pred(string pred) {
-    template Pred(Stuff...) {
-        alias Stuff[0] A;
-        enum Pred = mixin(pred);
+    template Pred(Stuff...)
+    if(Stuff.length == 1)
+    {
+        alias A = Stuff[0];
+        alias Pred = Alias!(mixin(pred));
     }
 }
 
-template Not(alias Pred) {
-    template Not(Stuff...) {
-        enum Not = !Pred!(Stuff[0]);
-    }
+//Stolen from phobos:
+/*
+ * With the builtin alias declaration, you cannot declare
+ * aliases of, for example, literal values. You can alias anything
+ * including literal values via this template.
+ */
+// symbols and literal values
+template Alias(alias a)
+{
+    static if (__traits(compiles, { alias x = a; }))
+        alias Alias = a;
+    else static if (__traits(compiles, { enum x = a; }))
+        enum Alias = a;
+    else
+        static assert(0, "Cannot alias " ~ a.stringof);
+}
+// types and tuples
+template Alias(a...)
+{
+    alias Alias = a;
 }
 
+unittest
+{
+    enum abc = 1;
+    static assert(__traits(compiles, { alias a = Alias!(123); }));
+    static assert(__traits(compiles, { alias a = Alias!(abc); }));
+    static assert(__traits(compiles, { alias a = Alias!(int); }));
+    static assert(__traits(compiles, { alias a = Alias!(1,abc,int); }));
+}
