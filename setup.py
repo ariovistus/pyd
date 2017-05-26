b9e508238c0f88e53520e99671b4c6e6829f9589
ariovistus <ellery-newcomer@utulsa.edu>
2017-05-25 22:37:43 -0700

    bump version

--- setup.py
+++ setup.py
@@ -72,6 +72,8 @@ distutils.core.setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
 
