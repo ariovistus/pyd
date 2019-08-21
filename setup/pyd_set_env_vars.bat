8c4cf4586041b3489a4f31118eb7beede8f89249
John Colvin <john.loughran.colvin@gmail.com>
2019-08-21 16:03:07 +0100

    spaces -> tabs

--- setup/pyd_set_env_vars.bat
+++ setup/pyd_set_env_vars.bat
@@ -1,10 +1,10 @@
 @echo off
 
 if "%~1"=="" (
-        echo python interpreter not specified, using ""python""
-        set PYD_PYTHON=python
+	echo python interpreter not specified, using ""python""
+	set PYD_PYTHON=python
 ) else (
-        set PYD_PYTHON=%~1
+	set PYD_PYTHON=%~1
 )
 
 "%PYD_PYTHON%" "%~dp0\pyd_get_env_set_text.py" > tmp_pyd_env_set.bat
