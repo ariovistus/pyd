d69869faea31acd0d632621d8b8f3140070ada53
John Colvin <john.loughran.colvin@gmail.com>
2019-08-21 16:01:42 +0100

    more careful quoting of paths in pyd_set_env_vars.bat

--- setup/pyd_set_env_vars.bat
+++ setup/pyd_set_env_vars.bat
@@ -1,13 +1,13 @@
 @echo off
 
-if "%1"=="" (
-	echo python interpreter not specified, using ""python""
-	set PYD_PYTHON=python
+if "%~1"=="" (
+        echo python interpreter not specified, using ""python""
+        set PYD_PYTHON=python
 ) else (
-	set PYD_PYTHON=%1
+        set PYD_PYTHON=%~1
 )
 
-%PYD_PYTHON% %~dp0\pyd_get_env_set_text.py > tmp_pyd_env_set.bat
+"%PYD_PYTHON%" "%~dp0\pyd_get_env_set_text.py" > tmp_pyd_env_set.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 call tmp_pyd_env_set.bat
 set result=%errorlevel%
