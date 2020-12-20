3085b7a8aa24acbf76e86a1bfbc0fd0c3f7736b6
mingwugmail <mingwu@gmail.com>
2020-12-20 00:31:24 -0500

    add work-around

--- examples/dub/simple_embedded/source/hello.d
+++ examples/dub/simple_embedded/source/hello.d
@@ -11,7 +11,14 @@ void main() {
     writeln(py_eval!string("'1 + %s' % 2"));
 
     auto c = py_eval("complex(2,-1)");
-    auto h = c.__hash__();
+    /* https://github.com/ariovistus/pyd/pull/143#issuecomment-748557221
+       there is no way to get opDispatch to distinguish between obj.property and obj.property().
+       So we have this kind of compromise where no parameters are treated as obj.property and
+       presence of parameters are treated as a method call.
+     */
+    auto hm = c.__hash__;      // return <method-wrapper '__hash__' of complex object at 0x7fa8c49897d0>
+    auto hr = c.__hash__()();  // return the result of the function call.
     writeln(c);
-    writeln(h);
+    writeln(hm);
+    writeln(hr);
 }
